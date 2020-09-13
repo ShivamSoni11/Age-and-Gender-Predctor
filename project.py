a2dbb3c8384f8a3933686550a3368bef37ce0c3f
ShivamSoni11 <64581581+ShivamSoni11@users.noreply.github.com>
2020-09-13 14:19:10 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,103 @@
+import argparse
+import cv2
+from tkinter import *
+def video():
+    def highlightFace(net, frame, conf_threshold=0.7):
+        frameOpencvDnn = frame.copy()
+        frameHeight = frameOpencvDnn.shape[0]
+        frameWidth = frameOpencvDnn.shape[1]
+        blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+        net.setInput(blob)
+        detections = net.forward()
+        faceBoxes = []
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > conf_threshold:
+                x1 = int(detections[0, 0, i, 3] * frameWidth)
+                y1 = int(detections[0, 0, i, 4] * frameHeight)
+                x2 = int(detections[0, 0, i, 5] * frameWidth)
+                y2 = int(detections[0, 0, i, 6] * frameHeight)
+                faceBoxes.append([x1, y1, x2, y2])
+                cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (185, 255, 147), int(round(frameHeight / 150)), 8)
+        return frameOpencvDnn, faceBoxes
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image')
+
+    args = parser.parse_args()
+
+    faceProto = "opencv_face_detector.pbtxt"
+    faceModel = "opencv_face_detector_uint8.pb"
+    ageProto = "age_deploy.prototxt"
+    ageModel = "age_net.caffemodel"
+    genderProto = "gender_deploy.prototxt"
+    genderModel = "gender_net.caffemodel"
+
+    MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+    ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+    genderList = ['Male', 'Female']
+
+    faceNet = cv2.dnn.readNet(faceModel, faceProto)
+    ageNet = cv2.dnn.readNet(ageModel, ageProto)
+    genderNet = cv2.dnn.readNet(genderModel, genderProto)
+
+    video = cv2.VideoCapture(args.image if args.image else 0)
+    padding = 20
+    while cv2.waitKey(1) < 0:
+        hasFrame, frame = video.read()
+        if not hasFrame:
+            cv2.waitKey()
+            break
+
+        resultImg, faceBoxes = highlightFace(faceNet, frame)
+        if not faceBoxes:
+            print("No face detected")
+
+        for faceBox in faceBoxes:
+            face = frame[max(0, faceBox[1] - padding):
+                         min(faceBox[3] + padding, frame.shape[0] - 1), max(0, faceBox[0] - padding)
+                                                                        :min(faceBox[2] + padding, frame.shape[1] - 1)]
+
+            blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+            genderNet.setInput(blob)
+            genderPreds = genderNet.forward()
+            gender = genderList[genderPreds[0].argmax()]
+            print(f'Gender: {gender}')
+
+            ageNet.setInput(blob)
+            agePreds = ageNet.forward()
+            age = ageList[agePreds[0].argmax()]
+            print(f'Age: {age[1:-1]} years')
+
+            cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
+                        (0, 255, 255), 2, cv2.LINE_AA)
+            cv2.imshow("Detecting age and gender", resultImg)
+
+
+main_application = Tk()
+main_application.geometry('400x160')
+main_application.title('Age and Gender Detector')
+main_application.wm_iconbitmap('icon.ico')
+
+logo = PhotoImage(file="img/shi.png")
+w1 = Label(main_application, image=logo)
+w1.place(x=335, y=10)
+
+
+w2 = Label(main_application, text="Check Yourself!!", font=("Times New Roman", 16), bg="#49A")
+w2.place(x=10, y=10)
+
+w3 = Label(main_application, text="!Predict your age!",fg="red", font=('fangsongti', 8))
+w3.place(x=153, y=65)
+
+w4 = Label(main_application, text="-by Shivam Soni",fg="black", font=("Helvetica", 6), bg="#49A")
+w4.place(x=335, y=135)
+
+btn = Button(main_application, text = "Let's Go", bd = '5',
+                          command = video)
+# Set the position of button on the top of window.
+btn.place(x=170, y=100)
+main_application['bg'] = '#49A'
+
+mainloop()
